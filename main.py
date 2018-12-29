7dfc1ac4bfbea4f53c380c692a0ef1df2aa4cb43
liuhengqiang <liiuhengqiang@jd.com>
2018-12-29 10:34:27 +0800

    修改book

--- main.py
+++ main.py
@@ -5,49 +5,30 @@ from bookTicket import *
 from time import sleep
 import multiprocessing
 
-def book1():
-    '''
-    根据坐席类别订票
-    :return:
-    '''
-    client = Login()
-    client.userLogin()
-    bt = BookTicket()
-    flag = True
-    while flag:
-        try:
-            if bt.bookTickets('刘恒强', '2019-01-24', '北京','武汉', 'M'):
-                break
-        except:
-            pass
-
-def book2(names):
+def book(names, date, fromStation, toStation, seatType, trainNames):
     '''
     根据坐席类别和车次订票
     :return:
     '''
-    # client = Login()
-    # client.userLogin()
-    # sleep(60)
     bt = BookTicket()
     flag = True
     while flag:
-        sleep(2)
+        sleep(1)
         try:
-            if bt.bookTickets(names, '2019-01-22', '北京','武汉', 'O', ['G81', 'G309', 'G507']):
+            if bt.bookTickets(names, date, fromStation, toStation, seatType, trainNames):
                 break
         except Exception as e:
             print(e)
             pass
 
-
 if __name__ == '__main__':
     client = Login()
     client.userLogin()
-    book2(['刘恒强', '张琳乐'])
+    book(['刘恒强', '张琳乐'], '2019-01-27', '北京', '武汉', 'O', ['G81', 'G79', 'G421', 'G405', 'G487', 'G507'])
+    # book(['刘恒强', '张琳乐'], '2019-01-27', '武汉', '白沙铺', 'O', ['D3245', 'D3289'])
     # book2('李四')
-    # p1 = multiprocessing.Process(target=book2, args=('刘恒强',))
-    # p2 = multiprocessing.Process(target=book2, args=('张琳乐',))
+    # p1 = multiprocessing.Process(target=book, args=('刘恒强',))
+    # p2 = multiprocessing.Process(target=book, args=('张琳乐',))
     #
     # p1.daemon = True
     # p2.daemon = True
